4e1d05d2d36e9aa124f7f861511a3af59338e2da
saimanoj1234 <67778413+saimanoj1234@users.noreply.github.com>
2020-11-07 11:33:01 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,16 +1,14 @@
 import cv2
 img = cv2.imread('HappyFish.JPG', 1)
-scale = 60 # for resizing images we do Downscale(Decrement in the size of the image) and Increment and resize either height or width and both
+scale = 60 #1.for resizing images we do Downscale(Decrement in the size of the image) and Increment and resize either height or width and both
 width = int(img.shape[1] * scale / 100)
 height = int(img.shape[0] * scale / 100)
 dim = (width, height)
 
-# for downscale take scale<100 and for increment take scale >100 i.e 400
-
-
-
+#2,for downscale take scale<100 and for increment take scale >100 i.e 400
 print('Original Dimensions : ', img.shape) # (194, 259, 3)
-# resize image
+
+#3.Resize image
 resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 print('Resized Dimensions : ', resized.shape) #(116, 155, 3)
